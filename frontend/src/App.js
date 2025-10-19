639df80e8909927ff7315d397bdafbd8f5494011
leonidvolyo <leonidvolyo@gmail.com>
2025-10-19 23:10:40 +0200

    More edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,12 @@ import { Toaster } from './components/ui/sonner';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/viridata-web">
+      {/* No basename when serving at https://viridata.eu/ */}
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* optional: catch-all redirect if you add more routes later */}
+          {/* <Route path="*" element={<Home />} /> */}
         </Routes>
       </BrowserRouter>
       <Toaster />
